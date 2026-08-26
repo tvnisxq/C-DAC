d53aee862aa4d2d78ca72e692a66819364b43f06
Tanishq Sharma <ponasanthem@gmail.com>
2026-08-26 18:44:28 +0530

    Added first python code at C-DAC BLR

---
+++ Python/Day 1/ex_01_hello.py
@@ -0,0 +1,8 @@
+# This is the my first python program
+print("Hello, World!")
+
+#* Comments
+# In python the comments usually start from a hash.
+
+print("This is a single line comment")
+# Single Line comment
