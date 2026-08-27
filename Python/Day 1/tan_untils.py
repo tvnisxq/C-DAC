1dcc60964bc464126ee4053da32fa3237a8c1617
Tanishq Sharma <ponasanthem@gmail.com>
2026-08-27 23:56:52 +0530

    Added sample code for functions

---
+++ Python/Day 1/tan_untils.py
@@ -0,0 +1,18 @@
+"""
+This is a utility module developed by Tanishq Sharma
+"""
+def hello():
+    "An example function to say Hello!"
+    print("Hello Tanishq")
+
+def hello():
+    "An example function to say Hello!"
+    print("Hello Priya")
+
+def hello():
+    "An example function to say Hello!"
+    print("Hello Friends")
+
+hello()
+
+
