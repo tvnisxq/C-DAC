9c2cc06984f2cb100985ef571630b7c4288b9080
Tanishq Sharma <ponasanthem@gmail.com>
2026-08-28 01:14:46 +0530

    Exercise 4: Odd or Even

---
+++ Python/Day 1/Assignments/odd_or_even.py
@@ -0,0 +1,9 @@
+def main():
+    num = int(input("Enter a number: "))
+
+    if num % 2 == 0:
+        print(f"{num} is an Even Number")
+    else:
+        print(f"{num} is an Odd Number")
+
+main()
