b3517a494e37f423e26e74f6fddf1f50862cb2c2
Tanishq Sharma <ponasanthem@gmail.com>
2026-09-02 23:26:19 +0530

    Modified ex_02.py

--- Python/Day6/ex_02.py
+++ Python/Day6/ex_02.py
@@ -1,3 +1,6 @@
+# Instance Variables: Defined inside methods (usually __init__) prefixed with self.. They belong to a specific object instance.
+# Class Variables: Defined directly inside the class body but outside any methods. They are shared across all instances of the class.
+
 class CDACStudents():
     course = "PGCP-BDA"
 
