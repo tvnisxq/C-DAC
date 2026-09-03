fa659b79577dcea091a51ffcf241068d284a74f8
Tanishq <DBDA32@ACTS.CDACECBLR.COM>
2026-09-03 11:38:57 +0530

    The Wizard's magic bag

--- Python/Day3/ex01.py
+++ Python/Day3/ex01.py
@@ -1,1 +1,16 @@
-# test code
+items = ["staff", "potion", "spellbook"]
+
+print("Portal transition activated!")
+
+# Get the new item from the user
+new_item = input("Enter new item: ")
+
+# Add the new item to the end
+items.append(new_item)
+
+# Remove the oldest item (index 0)
+ejected = items.pop(0)
+
+# Print the results
+print("Ejected oldest item:", ejected)
+print("Current items in the magic bag:", items)
